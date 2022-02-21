3e2550fc4459ac1ea370a0aa840edab78d2a77a1
VirgiJM <99683603+VirgiJM@users.noreply.github.com>
2022-02-21 19:55:05 +0100

    Update Circunferencia.java

--- Refactor/src/figures/Circunferencia.java
+++ Refactor/src/figures/Circunferencia.java
@@ -1,26 +1,26 @@
 package figures;
-
-import utilitats.CÌrculo;
+a
+import utilitats.C√≠rculo;
 
 public class Circunferencia {
-	private double r‡dio;
+	private double r√†dio;
 
 	public Circunferencia(double radio) {
-		this.r‡dio = radio;
+		this.r√†dio = radio;
 	}
 
 	public void imprimir() {
 		String rojo = "rojo";
 		String color = rojo;
-		System.out.println("Di·metro: " + 2 * r‡dio);
+		System.out.println("Di√°metro: " + 2 * r√†dio);
 		System.out.println("Color: " + color);
-		double area1 = 2 * 3.1416 * r‡dio * r‡dio;
+		double area1 = 2 * 3.1416 * r√†dio * r√†dio;
 		double area = area1;
 		System.out.println(area);
 	}
 
 	public boolean esIgual(boolean considerar_Decimales, Circunferencia otro) {
-		double radio1 = this.r‡dio;
+		double radio1 = this.r√†dio;
 		double radio2 = otro.getRad();
 		if (considerar_Decimales) {
 			if (radio1 == radio2)
